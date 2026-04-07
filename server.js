9a35320adc24172dedb12f3b916e5f3cd3699a64
tariel-codesignal <tariel@codesignal.com>
2026-04-07 17:07:13 +0400

    fix express bug

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ app.get('/content', (req, res) => {
 if (isProduction) {
   const distPath = path.join(__dirname, 'dist');
   app.use(express.static(distPath));
-  app.get('*', (req, res) => {
+  app.get('*path', (req, res) => {
     res.sendFile(path.join(distPath, 'index.html'));
   });
 }
